708528feaf20655c9649c94767c80144a9051a1f
vic <vic@MBP.local>
2016-07-22 13:19:44 +0800

    update

--- UIKitExtension/UITableView.m
+++ UIKitExtension/UITableView.m
@@ -11,7 +11,7 @@
 
 @implementation UITableViewCell (Shortcuts)
 
-NSAPropertyRetainSetter(setReuseIdentifier, @"_reuseIdentifier")
+//NSAPropertyRetainSetter(setReuseIdentifier, @"_reuseIdentifier")
 
 + (instancetype)cellWithStyle:(UITableViewCellStyle)style reuseIdentifier:(id)reuseIdentifier {
     return [[[self alloc] initWithStyle:style reuseIdentifier:reuseIdentifier] autorelease];
